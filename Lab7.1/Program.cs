9a394627faf602bda76e229c66835cb9fc98e750
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-13 12:49:54 +0300

    Добавьте файлы проекта.

---
+++ Lab7.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double s1 = CalcS(3, 4, 5);
+            Console.Write("{0} ", s1);
+            double s2 = CalcS(5, 6, 7);
+            Console.WriteLine("{0:f2}", s2);
+            Console.WriteLine("{0:f2}", Math.Max(s1, s2));
+            Console.ReadKey();
+        }
+        static double CalcS(int x, int y, int z)
+        {
+            double p = (x + y + z) / 2;
+            double S = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            return S;
+        }
+    }
+}
---
+++ Lab7.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S, V;
+            GetParams(a, out S, out V);
+        }
+        static void GetParams(int a, out double S, out double V)
+        {
+            S = 6 * Math.Pow(a, 2);
+            V = Math.Pow(a, 3);
+            Console.WriteLine("{0} {1}", S, V); ;
+            Console.ReadKey();
+
+        }
+    }
+}
